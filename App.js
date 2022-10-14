248a7f89a68b5b17db96ac57946c1663e68437ae
윤태웅 <yuntaewoong@yuntaeung-ui-iMac.local>
2022-10-14 11:38:14 +0900

    settled launch screen for ios

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+const wait = timeout => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+};
+
+export default function App() {
+  const onRefresh = React.useCallback(() => {
+    wait(2000).then(() => SplashScreen.hide());
+  }, []);
+  useEffect(() => {
+    onRefresh();
+  });
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
